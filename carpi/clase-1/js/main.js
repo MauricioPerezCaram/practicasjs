6f9b712c0ecf1a2f580a2301fb2616e29efc82c5
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-02-15 19:27:29 -0300

    first commit

---
+++ carpi/clase-1/js/main.js
@@ -0,0 +1,29 @@
+console.log("Hola we");
+
+let nombre = "Mauricio";
+
+let apellido = "Perez Caram";
+
+console.log(nombre, apellido);
+
+nombre = "Alfredo";
+
+console.log(nombre, apellido);
+
+let nombreApellido = nombre + " " + apellido;
+
+console.log(nombreApellido);
+
+let anoActual = 2024;
+
+const anoNacimiento = 1996;
+
+let edad = anoActual - anoNacimiento;
+
+console.log("Este año cumplis " + edad);
+
+let nombreUsuario = prompt("Ingresa tu nombre");
+
+console.log("El nombre del usuario es " + nombreUsuario);
+
+alert(33 + 33);
