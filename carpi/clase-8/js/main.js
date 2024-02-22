b39aaafc02855dc8dedaa0d239a42a051406104c
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-02-22 14:31:13 -0300

    eventos

---
+++ carpi/clase-8/js/main.js
@@ -0,0 +1,14 @@
+// const contenido = document.getElementById("contenido");
+// console.log(contenido);
+
+// const parrafos = document.getElementsByClassName("parrafo");
+// console.log(parrafos);
+
+//USAR QUERY SLECTOR
+const contenido = document.querySelector(".contenido");
+const parrafos = document.querySelectorAll(".parrafo");
+
+console.log(contenido.innerHTML);
+console.log(contenido.innerText);
+
+console.log(parrafos);
